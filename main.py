3ebf70200d9db8c2085c9a43037f27932cfed5ef
LanlanMC <Langrowup@outlook.com>
2024-02-06 20:26:54 +0800

    GUI complete

---
+++ main.py
@@ -0,0 +1,4 @@
+import ui
+
+app = ui.MainWindow()
+app.mainloop()
---
+++ ui.py
@@ -0,0 +1,124 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+
+
+class MainWindow(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.geometry("644x400+512+512")
+        self.title("PyAutoClicker")
+        self.resizable(False, False)
+        self.deiconify()
+
+        # Variables
+        self.button_text = tk.StringVar(value="开始")
+
+        # Setup UI
+        self.style = ttk.Style()
+        self.style.theme_use("vista")
+
+        self.click_interval_frame = ClickIntervalFrame(self, width=300, height=300)
+        self.click_interval_frame.place(x=16, y=12)
+
+        self.settings_frame = SettingFrame(self, width=312, height=160)
+        self.settings_frame.place(x=324, y=12)
+
+        TipsFrame(self, width=312, height=160).place(x=324, y=180)
+
+        self.hint_label = ttk.Label(self, text="过低的点击间隔可能导致鼠标卡顿。")
+        self.hint_label.place(x=12, y=320)
+
+        self.start_button = ttk.Button(self, textvariable=self.button_text)
+        self.start_button.place(x=400, y=352)
+
+        self.auto_tap1 = self.settings_frame.auto_tap1
+        self.auto_tap2 = self.settings_frame.auto_tap2
+        self.seperate_control = self.click_interval_frame.seperate_control
+        self.all_interval = self.click_interval_frame.all_interval
+        self.left_interval = self.click_interval_frame.left_interval
+        self.right_interval = self.click_interval_frame.right_interval
+
+
+class ClickIntervalFrame(ttk.LabelFrame):
+    def __init__(self, master, *, width, height, labelwidget=None):
+        if labelwidget is None:
+            super().__init__(master, width=width, height=height, text="点击间隔")
+        else:
+            super().__init__(master, width=width, height=height, labelwidget=labelwidget)
+
+        # Variables
+        self.seperate_control = tk.BooleanVar(value=False)
+        self.all_interval = tk.IntVar(value=100)
+        self.left_interval = tk.IntVar(value=100)
+        self.right_interval = tk.IntVar(value=100)
+
+        # Define widgets
+        self.all_desc_label = ttk.Label(self, text="间隔:")
+
+        self.sep_checkbox = ttk.Checkbutton(self, variable=self.seperate_control, text="分别控制")
+        self.all_spinbox = ttk.Spinbox(self, from_=0, to=5000, increment=5, textvariable=self.all_interval, width=7)
+        self.interval_frame = ttk.LabelFrame(self, width=272, height=172, labelwidget=self.sep_checkbox)
+        self.left_spinbox = ttk.Spinbox(self.interval_frame, from_=0, to=5000, increment=5,
+                                        textvariable=self.left_interval, width=7)
+        self.right_spinbox = ttk.Spinbox(self.interval_frame, from_=0, to=5000, increment=5,
+                                         textvariable=self.right_interval, width=7)
+
+        self.all_unit_label = ttk.Label(self, text="ms")
+        self.left_unit_label = ttk.Label(self.interval_frame, text="ms")
+        self.right_unit_label = ttk.Label(self.interval_frame, text="ms")
+
+        self.left_desc_label = ttk.Label(self.interval_frame, text="左键:")
+        self.right_desc_label = ttk.Label(self.interval_frame, text="右键:")
+
+        # Place widgets
+        self.all_desc_label.place(x=12, y=12)
+
+        self.all_spinbox.place(x=64, y=12)
+        self.interval_frame.place(x=12, y=68)
+        self.left_spinbox.place(x=96, y=0)
+        self.right_spinbox.place(x=96, y=32)
+
+        self.all_unit_label.place(x=160, y=12)
+        self.left_unit_label.place(x=192, y=0)
+        self.right_unit_label.place(x=192, y=32)
+
+        self.left_desc_label.place(x=32, y=0)
+        self.right_desc_label.place(x=32, y=32)
+
+
+class SettingFrame(ttk.LabelFrame):
+    def __init__(self, master, *, width, height, labelwidget=None):
+        if labelwidget is None:
+            super().__init__(master, width=width, height=height, text="设置")
+        else:
+            super().__init__(master, width=width, height=height, labelwidget=labelwidget)
+
+        # Variables
+        self.auto_tap1 = tk.BooleanVar(value=False)
+        self.auto_tap2 = tk.BooleanVar(value=False)
+
+        # Defin widgets
+        self.auto_tap1_checkbox = ttk.Checkbutton(self, variable=self.auto_tap1, text="自动切换武器")
+        self.auto_tap2_checkbox = ttk.Checkbutton(self, variable=self.auto_tap2, text="自动切换方块")
+
+        self.auto_tap1_desc_label = ttk.Label(self, text="在左键时自动切换到武器（按下\"1\")")
+        self.auto_tap2_desc_label = ttk.Label(self, text="在右键时自动切换到方块（按下\"2\")")
+
+        # Place widgets
+        self.auto_tap1_checkbox.place(x=12, y=0)
+        self.auto_tap2_checkbox.place(x=12, y=64)
+
+        self.auto_tap1_desc_label.place(x=12, y=28)
+        self.auto_tap2_desc_label.place(x=12, y=92)
+
+
+class TipsFrame(tk.LabelFrame):
+    """此组件是常量，在运行时无变化"""
+
+    def __init__(self, master, *, width, height, labelwidget=None):
+        if labelwidget is None:
+            super().__init__(master, width=width, height=height, text="提示")
+        else:
+            super().__init__(master, width=width, height=height, labelwidget=labelwidget)
+
+        # Setup UI

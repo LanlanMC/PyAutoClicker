803aeb4eb53092e5ba07e75477f05d4d56ee11ee
LanlanMC <Langrowup@outlook.com>
2024-02-06 17:13:21 +0800

    Initial commit

---
+++ keybd.py
@@ -0,0 +1,32 @@
+import ctypes
+
+keyboardMapping = {'1': 49, '2': 50, '3': 51, '4': 52, '5': 53, '6': 54, '7': 55, '8': 56, '9': 57}
+
+
+def keyDown(key: str) -> None:
+    mods, vkCode = divmod(keyboardMapping[key], 0x100)
+
+    for apply_mod, vk_mod in [(mods & 4, 0x12), (mods & 2, 0x11), (mods & 1, 0x10)]:
+        if apply_mod:
+            ctypes.windll.user32.keybd_event(vk_mod, 0, 0x0000, 0)
+    ctypes.windll.user32.keybd_event(vkCode, 0, 0x0000, 0)
+    for apply_mod, vk_mod in [(mods & 1, 0x10), (mods & 2, 0x11), (mods & 4, 0x12)]:
+        if apply_mod:
+            ctypes.windll.user32.keybd_event(vk_mod, 0, 0x0002, 0)
+
+
+def keyUp(key: str) -> None:
+    mods, vkCode = divmod(keyboardMapping[key], 0x100)
+
+    for apply_mod, vk_mod in [(mods & 4, 0x12), (mods & 2, 0x11), (mods & 1, 0x10)]:
+        if apply_mod:
+            ctypes.windll.user32.keybd_event(vk_mod, 0, 0x0002, 0)
+    ctypes.windll.user32.keybd_event(vkCode, 0, 0x0000, 0)
+    for apply_mod, vk_mod in [(mods & 1, 0x10), (mods & 2, 0x11), (mods & 4, 0x12)]:
+        if apply_mod:
+            ctypes.windll.user32.keybd_event(vk_mod, 0, 0x0002, 0)
+
+
+def keyTap(key: str) -> None:
+    keyDown(key)
+    keyUp(key)
---
+++ mouse.py
@@ -0,0 +1,70 @@
+import ctypes
+
+from ctypes import windll
+
+__all__ = ["left_click", "right_click"]
+
+
+class MouseInput(ctypes.Structure):
+    """鼠标输入结构体"""
+    _fields_ = [("dx", ctypes.c_long),
+                ("dy", ctypes.c_long),
+                ("mouseData", ctypes.c_ulong),
+                ("dwFlags", ctypes.c_ulong),
+                ("time", ctypes.c_ulong),
+                ("dwExtraInfo", ctypes.POINTER(ctypes.c_ulong))]
+
+
+class Input(ctypes.Structure):
+    """输入结构体"""
+
+    class _INPUT(ctypes.Union):
+        _fields_ = [("mi", MouseInput)]
+
+    _anonymous_ = ("_input",)
+    _fields_ = [("type", ctypes.c_ulong),
+                ("_input", _INPUT)]
+
+
+MOUSE_EVENT_LEFT_DOWN = 0x0002
+MOUSE_EVENT_LEFT_UP = 0x0004
+MOUSE_EVENT_RIGHT_DOWN = 0x0008
+MOUSE_EVENT_RIGHT_UP = 0x0010
+
+SendClick = windll.user32.SendInput
+SendClick.argtypes = (ctypes.c_uint, ctypes.POINTER(Input), ctypes.c_int)
+SendClick.restype = ctypes.c_uint
+
+
+def left_click():
+    """左键单击"""
+    mouse_down = Input()
+    mouse_down.type = 0
+    mouse_down.mi.dwFlags = MOUSE_EVENT_LEFT_DOWN
+
+    mouse_up = Input()
+    mouse_up.type = 0
+    mouse_up.mi.dwFlags = MOUSE_EVENT_LEFT_UP
+
+    events = (Input * 2)()
+    events[0] = mouse_down
+    events[1] = mouse_up
+
+    SendClick(2, events, ctypes.sizeof(Input))
+
+
+def right_click():
+    """右键单击"""
+    mouse_down = Input()
+    mouse_down.type = 0
+    mouse_down.mi.dwFlags = MOUSE_EVENT_RIGHT_DOWN
+
+    mouse_up = Input()
+    mouse_up.type = 0
+    mouse_up.mi.dwFlags = MOUSE_EVENT_RIGHT_UP
+
+    events = (Input * 2)()
+    events[0] = mouse_down
+    events[1] = mouse_up
+
+    SendClick(2, events, ctypes.sizeof(Input))
